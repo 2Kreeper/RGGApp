427fb04901390170cca0fef615bd13787e3b28c5
Alex <alexander@baron-online.eu>
2018-08-31 18:28:14 +0200

    Swipe Refresh for events
    Implemented swipe refresh for events fragment, also removed unused EventsFragment & TrainingTimesFragment clone

--- app/src/main/java/eu/barononline/rggapp/fragments/events/EventsFragment.java
+++ app/src/main/java/eu/barononline/rggapp/fragments/events/EventsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,6 +22,7 @@ import eu.barononline.rggapp.services.IReceiver;
 public class EventsFragment extends Fragment implements IReceiver<Event[]> {
 
 	private RecyclerView recyclerView;
+	private SwipeRefreshLayout swipeRefreshLayout;
 
 	@Nullable
 	@Override
@@ -33,11 +35,22 @@ public class EventsFragment extends Fragment implements IReceiver<Event[]> {
 		recyclerView.setLayoutManager(llm);
 		recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
 
-		new EventListFetcher(this).execute();
+		swipeRefreshLayout = root.findViewById(R.id.swipe_to_refresh);
+		swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				loadEvents();
+			}
+		});
 
+		loadEvents();
 		return root;
 	}
 
+	private void loadEvents() {
+		new EventListFetcher(this).execute();
+		swipeRefreshLayout.setRefreshing(false);
+	}
 
 	@Override
 	public void onReceive(Event[] obj) {
